ea80e437f10961c26e2798af3c0bcae7a3bbf782
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-09 01:55:36 +0200

    setup the base configurations.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,7 @@ import MovieThumb from "../elements/MovieThumb/MovieThumb";
 import Spinner from "../elements/Spinner/Spinner";
 import LoadMoreBtn from "../elements/LoadMoreBtn/LoadMoreBtn";
 import './Home.css';
+import {API_KEY,API_URL,IMAGE_BASE_URL,POSTER_SIZE,BACKDROP_SIZE} from "../../config";
 
 class Home extends Component {
     state = {
