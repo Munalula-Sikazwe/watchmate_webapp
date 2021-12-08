50b3055540c120186127d5fdd3430ccfa8501d85
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-08 23:42:43 +0200

    created FourColGrid and MovieThumb components.

---
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'FourColGrid.css';
+
+const fourColGrid = (props)=>{
+    return(
+        <div>
+            FourColGrid
+        </div>
+    )
+}
+
+export default fourColGrid;
---
+++ src/components/elements/HeroImage/HeroImage.js
@@ -0,0 +1,12 @@
+import  React from 'react';
+import './HeroImage.css';
+
+const heroImage = (props)=>{
+    return(
+        <div>
+            HeroImage
+        </div>
+    )
+}
+
+export default heroImage;
