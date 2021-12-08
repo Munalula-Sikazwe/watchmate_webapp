6d4dcfa36e04a95eef69729bae75b98e03f959d7
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-09 00:09:00 +0200

    fixed typo fourcolgrid component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,8 @@ import Header from '../elements/Header/Header';
 import SearchBar from "../elements/SearchBar/SearchBar";
 import HeroImage from "../elements/HeroImage/HeroImage";
 import FourColGrid from "../elements/FourColGrid/FourColGrid";
+import MovieThumb from "../elements/MovieThumb/MovieThumb";
+import Spinner from "../elements/Spinner/Spinner";
 import  Home from '../Home/Home';
 
 const app = () => {
