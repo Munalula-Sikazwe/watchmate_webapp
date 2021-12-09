e74019bbc6ee1c7f6a8b7d83cedd4da51cd5782d
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-09 05:37:10 +0200

    created the search Items method.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -28,39 +28,61 @@ class Home extends Component {
     fetchItems = (endpoint) => {
         fetch(endpoint)
             .then(result => result.json())
-            .then(result =>{
+            .then(result => {
                 this.setState({
-                    movies:[...this.state.movies,result.results],
+                    movies: [...this.state.movies, result.results],
                     heroImage: this.state.heroImage || result.results[0],
-                    loading:false,
+                    loading: false,
                     currentPage: result.page,
-                    totalPages:result.total_pages,
+                    totalPages: result.total_pages,
 
 
                 })
             })
 
     }
-    loadMoreItems = ()=>{
+    loadMoreItems = () => {
         let endpoint = '';
         this.setState({
-            loading:true
+            loading: true
         })
-        if (this.state.searchTerm === ''){
+        if (this.state.searchTerm === '') {
             endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
-        }
-        else{
-            endpoint =  `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1&query=${this.state.searchTerm}`;
+        } else {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1&query=${this.state.searchTerm}`;
 
         }
-       this.fetchItems(endpoint);
+        this.fetchItems(endpoint);
+    }
+    searchItems = (searchTerm) => {
+        let endpoint = '';
+        this.setState({
+            movies: [],
+            loading: true,
+            searchTerm
+        })
+        if (searchTerm === '') {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        } else {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1&query=${this.state.searchTerm}`;
+        }
+        this.fetchItems(endpoint)
     }
 
     render() {
         return (
-            <div>
-                <HeroImage/>
-                <SearchBar/>
+
+            <div className='rmdb-home'>
+                {this.state.heroImage ?
+                    <div>
+                        <HeroImage image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
+                                   title={this.state.heroImage.original_title}
+                                   text={this.state.heroImage.overview}
+                        />
+                        <SearchBar/>
+                    </div>
+                    : null}
+
                 <FourColGrid/>
                 <Spinner/>
                 <LoadMoreBtn/>
