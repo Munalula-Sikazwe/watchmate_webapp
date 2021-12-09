4dc7fa08a6b87eca4e4ff84133e7d864e86b40cc
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-09 02:50:51 +0200

    successfully able to retrieve data from api.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import  React , {Component } from 'react';
+import React, {Component} from 'react';
 import SearchBar from "../elements/SearchBar/SearchBar";
 import HeroImage from "../elements/HeroImage/HeroImage";
 import FourColGrid from "../elements/FourColGrid/FourColGrid";
@@ -6,19 +6,36 @@ import MovieThumb from "../elements/MovieThumb/MovieThumb";
 import Spinner from "../elements/Spinner/Spinner";
 import LoadMoreBtn from "../elements/LoadMoreBtn/LoadMoreBtn";
 import './Home.css';
-import {API_KEY,API_URL,IMAGE_BASE_URL,POSTER_SIZE,BACKDROP_SIZE} from "../../config";
+import {API_KEY, API_URL, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from "../../config";
 
 class Home extends Component {
     state = {
-        movies:[],
-        heroImage:null,
-        loading:false,
-        currentPage:0,
-        totalPages:0,
-        searchTerm:''
+        movies: [],
+        heroImage: null,
+        loading: false,
+        currentPage: 0,
+        totalPages: 0,
+        searchTerm: ''
     }
-    render(){
-        return(
+
+    componentDidMount() {
+        this.setState({loading: true})
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        this.fetchItems(endpoint)
+
+    }
+
+    fetchItems = (endpoint) => {
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result =>{
+                console.log(result)
+            })
+
+    }
+
+    render() {
+        return (
             <div>
                 <HeroImage/>
                 <SearchBar/>
@@ -29,4 +46,5 @@ class Home extends Component {
         )
     }
 }
+
 export default Home;
