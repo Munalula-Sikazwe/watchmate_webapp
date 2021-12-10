54f9c1b2bb5493950b044876d5533fd24f8d5392
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-11 00:58:42 +0200

    created routes to the various pages.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,20 @@
 import  React from 'react';
 import Header from '../elements/Header/Header';
-
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import  Home from '../Home/Home';
+import Movie from "../Movie/Movie";
 
 const app = () => {
     return (
-        <div>
-            <Header/>
-            <Home/>
-        </div>
+        <BrowserRouter>
+            <React.Fragment>
+                <Header/>
+                <Switch>
+                    <Route path='/' component={Home} exact/>
+                    <Route path='/:movieId' component={Movie}/>
+                </Switch>
+            </React.Fragment>
+        </BrowserRouter>
     )
 }
 
