31de77cfeda0db43d30f4ce4e0badaea98f12a34
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-10 23:39:15 +0200

    created actor bar component.

---
+++ src/components/elements/Actor/Actor.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {IMAGE_BASE_URL} from "../../../config";
+import 'Actor.css';
+
+const actor = (props)=>{
+    return (
+        <div>
+            actor
+        </div>
+    )
+}
+export default actor;
