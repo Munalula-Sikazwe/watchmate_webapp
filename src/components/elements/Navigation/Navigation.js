78c339fd46e9b4bb1253fad2d862935eb6402b6e
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-10 23:07:57 +0200

    created navigation and movie components.

---
+++ src/components/elements/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import './Navigation.css'
+
+const navigation = (props)=>{
+    return(
+        <div>
+            Navigation
+        </div>
+    )
+}
+export default navigation;
