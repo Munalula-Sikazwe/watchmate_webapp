1e355076514af8c910499f47a07846970cfe463b
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-10 23:32:20 +0200

    created movie info and movie info bar component.

---
+++ src/components/elements/MovieInfo/MovieInfo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {BACKDROP_SIZE,IMAGE_BASE_URL,POSTER_SIZE} from "../../../config";
+import FontAwesome from "react-fontawesome";
+import MovieThumb from "../MovieThumb/MovieThumb";
+import './MovieInfo.css';
+
+const movieInfo = (props)=>{
+    return (
+        <div>
+            MovieInfo
+        </div>
+    )
+}
+
+export default movieInfo;
---
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FontAwesome from "react-fontawesome";
+import {calcTime,convertMoney} from "../../../helpers";
+import 'MovieInfoBar.css';
+
+const movieInfoBar = (props)=>{
+    return (
+        <div>
+            MovieInfoBar
+        </div>
+    )
+}
+export default movieInfoBar;
