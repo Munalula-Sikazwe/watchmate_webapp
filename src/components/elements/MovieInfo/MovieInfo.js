72282380746e5e4bd6ae0026caaa9d9a207552ca
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-13 08:38:38 +0200

    set up the movie thumb and details.

--- src/components/elements/MovieInfo/MovieInfo.js
+++ src/components/elements/MovieInfo/MovieInfo.js
@@ -9,8 +9,27 @@ const movieInfo = (props)=>{
         <div className='rmdb-movieinfo' style={{
             background: props.movie.backdrop_path ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.movie.backdrop_path}')`:'#000'
         }}>
-            <h1> MovieInfo</h1>
+            <div className='rmdb-movieinfo-content'>
+                <div className='rmdb-movieinfo-thumb'>
+                    <MovieThumb
+                    image={props.movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.movie.poster_path}`:'.images/no_image.jpg'}
+                    clickable = {false}
+                    />
+                    <div className='rmdb-movieinfo-text'>
+                        <h1>{props.movie.title}</h1>
+                        <h3>PLOT</h3>
+                        <p>{props.movie.overview}</p>
+                <h3>RMDB Rating</h3>
+                        <div className='rmdb-rating'>
+                            <meter min='0' max='100' optimum='100' low='40' high='70' value={props.movie.vote_average * 10}></meter>
+                            <p className='rmdb-score'>{props.movie.vote_average}</p>
 
+                        </div>
+                        {props.directors > 1 ? <h3>Directors</h3>: <h3>Director</h3>}
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
