1a6f42daf4de1b8b9a8491a54aa9fdf23de858b2
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-13 21:47:24 +0200

    completed actor component.

--- src/components/elements/Actor/Actor.js
+++ src/components/elements/Actor/Actor.js
@@ -2,10 +2,15 @@ import React from "react";
 import {IMAGE_BASE_URL} from "../../../config";
 import './Actor.css';
 
-const actor = (props)=>{
+const actor = (props) => {
+    const POSTER_SIZE = 'w154'
     return (
-        <div>
-            actor
+        <div className='rmdb-actor'>
+            <img
+                src={props.actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}/${props.actor.profile_path}` : './images/no_image.jpg'}
+                alt='actorthumb'/>
+            <span className='rmdb-actor-name'>{props.actor.name}</span>
+            <span className='rmdb-actor-character'>{props.actor.character}</span>
         </div>
     )
 }
