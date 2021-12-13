9057882985dd77db8ede33fcf5be06a312dbe332
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-13 04:21:46 +0200

    added params to component.

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,5 +1,5 @@
-import React,{Component} from 'react';
-import {API_URL,API_KEY} from "../../config";
+import React, {Component} from 'react';
+import {API_URL, API_KEY} from "../../config";
 import Navigation from "../elements/Navigation/Navigation";
 import MovieInfo from "../elements/MovieInfo/MovieInfo";
 import MovieInfoBar from "../elements/MovieInfoBar/MovieInfoBar";
@@ -10,61 +10,61 @@ import "./Movie.css";
 
 class Movie extends Component {
     state = {
-        movies:null,
-        actors:null,
-        directors:[],
-        loading:false
+        movies: null,
+        actors: null,
+        directors: [],
+        loading: false
     }
+
     componentDidMount() {
         this.setState({
-            loading:true
+            loading: true
         })
-        const endpoint =   `${API_URL}movie/${this.props.match.movieId}?api_key=${API_KEY}&language=eng-US`;
+        const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=eng-US`;
         this.fetchItems(endpoint);
     }
-    fetchItems = (endpoint)=>{
+
+    fetchItems = (endpoint) => {
         fetch(endpoint)
             .then(result => result.json())
             .then(result => {
                 if (result.status_code) {
                     this.setState({
-                        loading:false
+                        loading: false
                     })
-                }
-                else {
+                } else {
                     this.setState({
-                        movies:result
-                    },
-                        ()=>{
-                          const endpoint = `${API_URL}movie/${this.props.match.movieId}/credits?api_key=${API_KEY}&language=eng-US`;
-                          fetch(endpoint)
-                              .then(result = result.json())
-                              .then(
-                                  result =>{
-                                      const directors = result.crew.filter((member)=> member.job === 'Director');
-                                      this.setState({
-                                          actor:result.cast,
-                                          directors,
-                                          loading:false
-                                      })
-                                  }
-                              )
-                              .catch((error)=>{
-                                  console.log({"Error":error})
-                              })
+                            movies: result
+                        },
+                        () => {
+                            const endpoint = `${API_URL}movie/${this.props.match.movieId}/credits?api_key=${API_KEY}&language=eng-US`;
+                            fetch(endpoint)
+                                .then(result = result.json())
+                                .then(
+                                    result => {
+                                        const directors = result.crew.filter((member) => member.job === 'Director');
+                                        this.setState({
+                                            actor: result.cast,
+                                            directors,
+                                            loading: false
+                                        })
+                                    }
+                                )
+                                .catch((error) => {
+                                    console.log({"Error": error})
+                                })
 
                         })
 
                 }
             })
     }
-    render = ()=>{
-        return(
+    render = () => {
+        return (
             <div className='rmdb-movie'>
                 <Navigation/>
                 <MovieInfo/>
                 <MovieInfoBar/>
-<FourColGrid/>
                 <Spinner/>
             </div>
         )
