44f467fccf7979684cdf77949b4a6be941d469b1
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-13 06:40:24 +0200

    created navigation bar links.

--- src/components/elements/Navigation/Navigation.js
+++ src/components/elements/Navigation/Navigation.js
@@ -4,8 +4,14 @@ import './Navigation.css'
 
 const navigation = (props)=>{
     return(
-        <div>
-            Navigation
+        <div className='rmdb-navigation'>
+            <div className='rmdb-navigation-content'></div>
+           <Link to='/'>
+               <p>Home</p>
+           </Link>
+            <p>/</p>
+           <p>{props.movie}</p>
+
         </div>
     )
 }
