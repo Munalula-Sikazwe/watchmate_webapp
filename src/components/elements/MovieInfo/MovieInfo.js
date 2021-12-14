877ce1d7e736b8d68b177aa993312755523db00c
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-12-14 05:18:55 +0200

    fixed rogue div in navigation component.

--- src/components/elements/MovieInfo/MovieInfo.js
+++ src/components/elements/MovieInfo/MovieInfo.js
@@ -1,38 +1,39 @@
 import React from "react";
-import {BACKDROP_SIZE,IMAGE_BASE_URL,POSTER_SIZE} from "../../../config";
+import {BACKDROP_SIZE, IMAGE_BASE_URL, POSTER_SIZE} from "../../../config";
 import FontAwesome from "react-fontawesome";
 import MovieThumb from "../MovieThumb/MovieThumb";
 import './MovieInfo.css';
 
-const movieInfo = (props)=>{
+const movieInfo = (props) => {
     return (
         <div className='rmdb-movieinfo' style={{
-            background: props.movie.backdrop_path ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.movie.backdrop_path}')`:'#000'
+            background: props.movie.backdrop_path ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.movie.backdrop_path}')` : '#000'
         }}>
             <div className='rmdb-movieinfo-content'>
                 <div className='rmdb-movieinfo-thumb'>
                     <MovieThumb
-                    image={props.movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.movie.poster_path}`:'.images/no_image.jpg'}
-                    clickable = {false}
+                        image={props.movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.movie.poster_path}` : '.images/no_image.jpg'}
+                        clickable={false}
                     />
-                    <div className='rmdb-movieinfo-text'>
-                        <h1>{props.movie.title}</h1>
-                        <h3>PLOT</h3>
-                        <p>{props.movie.overview}</p>
-                <h3>RMDB Rating</h3>
-                        <div className='rmdb-rating'>
-                            <meter min='0' max='100' optimum='100' low='40' high='70' value={props.movie.vote_average * 10}></meter>
-                            <p className='rmdb-score'>{props.movie.vote_average}</p>
-
-                        </div>
-                        {props.directors > 1 ? <h3>Directors</h3>: <h3>Director</h3>}
-                        {props.directors.map((element,i)=>{
-                            return <p key={i} className='rmdb-director'>{element.name}</p>
-                        })}
-                    </div>
-                    <FontAwesome className='fa-film' name='film' size='5x'/>
                 </div>
+                <div className='rmdb-movieinfo-text'>
+                    <h1>{props.movie.title}</h1>
+                    {console.log(props.movie.title)}
+                    <h3>PLOT</h3>
+                    <p>{props.movie.overview}</p>
+                    <h3>RMDB Rating</h3>
+                    <div className='rmdb-rating'>
+                        <meter min='0' max='100' optimum='100' low='40' high='70'
+                               value={props.movie.vote_average * 10}></meter>
+                        <p className='rmdb-score'>{props.movie.vote_average}</p>
 
+                    </div>
+                    {props.directors > 1 ? <h3>Directors</h3> : <h3>Director</h3>}
+                    {props.directors.map((element, i) => {
+                        return <p key={i} className='rmdb-director'>{element.name}</p>
+                    })}
+                </div>
+                <FontAwesome className='fa-film' name='film' size='5x'/>
             </div>
         </div>
     )
