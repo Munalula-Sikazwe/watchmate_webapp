7fb1a382c1623930839a3d737744927ad7ed2351
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-03-30 14:17:32 +0200

    refactored endpoint variable to  exist within the block scope

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -51,17 +51,17 @@ class Home extends Component {
 
     }
     loadMoreItems = () => {
-        let endpoint = '';
         this.setState({
             loading: true
         })
         if (this.state.searchTerm === '') {
-            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+           let endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+            this.fetchItems(endpoint);
         } else {
-            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}&query=${this.state.searchTerm}`;
-
+            let endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}&query=${this.state.searchTerm}`;
+            this.fetchItems(endpoint);
         }
-        this.fetchItems(endpoint);
+
     }
     searchItems = (searchTerm) => {
 
